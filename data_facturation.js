2bd9b64b7c983e30e43bd32ca4b4d64da7dd8f12
Roman Chparski <roman.chparski@inovatic.fr>
2025-03-12 15:01:46 +0100

    added static files

---
+++ data_facturation.js
@@ -0,0 +1,21 @@
+export const dummyFacturationData = [
+  { month: "Jan", totalFacturation: 50000, penalites: 10, tauxErreur: 0.8 },
+  { month: "Fév", totalFacturation: 60000, penalites: 12, tauxErreur: 1.2 },
+  { month: "Mar", totalFacturation: 70000, penalites: 8, tauxErreur: 0.9 },
+  { month: "Avr", totalFacturation: 75000, penalites: 15, tauxErreur: 1.5 },
+  { month: "Mai", totalFacturation: 80000, penalites: 20, tauxErreur: 1.1 },
+  { month: "Juin", totalFacturation: 85000, penalites: 22, tauxErreur: 0.7 },
+  { month: "Juil", totalFacturation: 90000, penalites: 18, tauxErreur: 0.9 },
+  { month: "Août", totalFacturation: 95000, penalites: 25, tauxErreur: 1.3 },
+  { month: "Sep", totalFacturation: 100000, penalites: 30, tauxErreur: 1.6 },
+  { month: "Oct", totalFacturation: 110000, penalites: 28, tauxErreur: 1.2 },
+  { month: "Nov", totalFacturation: 115000, penalites: 35, tauxErreur: 1.7 },
+  { month: "Déc", totalFacturation: 120000, penalites: 40, tauxErreur: 1.8 },
+];
+
+export const dummyPenalitesData = [
+  { type: "Retard traitement", count: 120 },
+  { type: "Correction saisie", count: 80 },
+  { type: "Correction urgente", count: 60 },
+  { type: "Taux erreur > 1%", count: 90 },
+];
